f364f3e435aca52d7931b9af3b0e9d39965263e2
dev41 <YGYebT2b>
2021-08-12 14:31:17 +0800

    新增 java.time.Duration 單元測試。

---
+++ src/test/java/jdk8/time/DurationClassTest.java
@@ -0,0 +1,19 @@
+package jdk8.time;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DurationClassTest {
+
+    @Test
+    void toMinutes_360Seconds_Return6Minutes() {
+        Duration seconds = Duration.ofSeconds(360);
+
+        long expected = 6;
+        long actual = seconds.toMinutes();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
