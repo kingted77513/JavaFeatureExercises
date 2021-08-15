c579e827aa8d9a892bed8ef29a0304f4d54bd538
kingted77513 <kingted77513@gmail.com>
2021-08-15 17:26:24 +0800

    新增 Strings in switch Statements 語法測試

---
+++ src/test/java/jdk7/SwitchStatementFeatureTest.java
@@ -0,0 +1,38 @@
+package jdk7;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SwitchStatementFeatureTest {
+    @Test
+    void getCaseByString() {
+        final String fruit = "apple";
+
+        final String excepted = getCaseByStringUsingBeforeJDK(fruit);
+        final String actual = getCaseByStringUsingJDK8(fruit);
+
+        Assertions.assertEquals(excepted, actual);
+
+    }
+
+    private String getCaseByStringUsingBeforeJDK(final String fruit) {
+        if (fruit.equals("apple")) {
+            return "This is an apple";
+        } else if (fruit.equals("banana")) {
+            return "This is a banana";
+        } else {
+            return null;
+        }
+    }
+
+    private String getCaseByStringUsingJDK8(final String fruit) {
+        switch (fruit) {
+            case "apple":
+                return "This is an apple";
+            case "banana":
+                return "This is a banana";
+            default:
+                return null;
+        }
+    }
+}
