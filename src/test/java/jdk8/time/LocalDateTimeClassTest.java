452c9aba95a3c752d992f2c6cc72ec43ef1d4508
dev41 <YGYebT2b>
2021-10-01 16:28:18 +0800

    新增 取得秒數 單元測試。

--- src/test/java/jdk8/time/LocalDateTimeClassTest.java
+++ src/test/java/jdk8/time/LocalDateTimeClassTest.java
@@ -1,8 +1,11 @@
 package jdk8.time;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.Calendar;
-
+import java.util.Date;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +23,13 @@ public class LocalDateTimeClassTest {
         int expected = Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
         int actual = LocalDateTime.now().getDayOfYear();
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void getTime() {
+        final LocalDate today = LocalDate.now();
+        final long startTime = Date.from(LocalDateTime.of(today, LocalTime.MIN).atZone(ZoneId.systemDefault()).toInstant()).getTime();
+        final long endTime = Date.from(LocalDateTime.of(today, LocalTime.MAX).atZone(ZoneId.systemDefault()).toInstant()).getTime();
+        Assertions.assertEquals(startTime, endTime, 100000000);
     }
 }
