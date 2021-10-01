98ea9a3f62e83947d6d9f91b3ec06c44dfedb175
dev41 <YGYebT2b>
2021-10-01 16:14:39 +0800

    新增 BigDecimal 的數字轉換和比大小單元測試。

--- src/test/java/jdk7/DecimalClassTest.java
+++ src/test/java/jdk7/DecimalClassTest.java
@@ -2,7 +2,6 @@ package jdk7;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +16,47 @@ public class DecimalClassTest {
         BigDecimal actual = BigDecimal.ONE.subtract(lendMoney.divide(deviceMoney, 2, RoundingMode.HALF_UP));
 
         Assertions.assertEquals(excepted, actual);
+    }
 
+    @Test
+    void toString_DecimalToString_ReturnDigitString() {
+        BigDecimal integer = BigDecimal.valueOf(10);
 
+        String excepted = "10";
+        String actual = integer.toString();
+        Assertions.assertEquals(excepted, actual);
+    }
+
+    @Test
+    void compareTo_TwoBigDecimal_ReturnNumberOfOver() {
+        int excepted = 1;
+
+        BigDecimal num1 = new BigDecimal(10);
+        BigDecimal num2 = new BigDecimal(5);
+
+        int actual = num1.compareTo(num2);
+        Assertions.assertEquals(excepted, actual);
+    }
+
+    @Test
+    void compareTo_TwoBigDecimal_ReturnNumberOfEqual() {
+        int excepted = 0;
+
+        BigDecimal num1 = new BigDecimal(10);
+        BigDecimal num2 = new BigDecimal(10);
+
+        int actual = num1.compareTo(num2);
+        Assertions.assertEquals(excepted, actual);
+    }
+
+    @Test
+    void compareTo_TwoBigDecimal_ReturnNumberOfLess() {
+        int excepted = -1;
+
+        BigDecimal num1 = new BigDecimal(5);
+        BigDecimal num2 = new BigDecimal(10);
+
+        int actual = num1.compareTo(num2);
+        Assertions.assertEquals(excepted, actual);
     }
 }
