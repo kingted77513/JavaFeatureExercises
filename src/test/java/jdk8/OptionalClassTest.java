5be5d06d5c8777e15784d13b59ba505c4c4ee35b
dev41 <YGYebT2b>
2021-10-01 16:23:58 +0800

    新增 Optional 取值的單元測試。

--- src/test/java/jdk8/OptionalClassTest.java
+++ src/test/java/jdk8/OptionalClassTest.java
@@ -1,49 +1,54 @@
 package jdk8;
 
+import java.math.BigDecimal;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class OptionalClassTest {
 
     @Test
-    void isPresent_WrapNullObject_ReturnFalse(){
+    void maxOfBigDecimal() {
+        BigDecimal limitAmount = BigDecimal.valueOf(0.5);
+        BigDecimal deviceMoney = BigDecimal.valueOf(0.8);
+
+        BigDecimal excepted = BigDecimal.valueOf(0.8);
+        BigDecimal actual = Optional.ofNullable(limitAmount).map(deviceMoney::max).orElse(BigDecimal.valueOf(0.0));
+        Assertions.assertEquals(excepted, actual);
+    }
+
+    @Test
+    void isPresent_WrapNullObject_ReturnFalse() {
         Optional<Object> optional = Optional.empty();
-        //noinspection ConstantConditions
         Assertions.assertFalse(optional.isPresent());
     }
 
     @Test
-    void isPresent_WrapNonNullObject_ReturnTrue(){
+    void isPresent_WrapNonNullObject_ReturnTrue() {
         Optional<Object> optional = Optional.of(new Object());
-        //noinspection ConstantConditions
         Assertions.assertTrue(optional.isPresent());
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     @Test
-    void of_GiveNullObjectInOf_ThrowNullPointerException(){
-        //noinspection ConstantConditions
+    void of_GiveNullObjectInOf_ThrowNullPointerException() {
         Assertions.assertThrows(NullPointerException.class, () -> Optional.of(null));
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     @Test
-    void get_GiveNullObjectInOf_ThrowNullPointerException(){
-        //noinspection ConstantConditions
+    void get_GiveNullObjectInOf_ThrowNullPointerException() {
         Assertions.assertThrows(NullPointerException.class, () -> Optional.of(null).get());
     }
 
     @Test
-    void get_GiveNullObjectInOfNullable_ReturnNoSuchElementException(){
-        //noinspection ConstantConditions
+    void get_GiveNullObjectInOfNullable_ReturnNoSuchElementException() {
         Assertions.assertThrows(NoSuchElementException.class, () -> Optional.ofNullable(null).get());
     }
 
     @Test
-    void get_GiveIntegerObjectInOf_ReturnIntegerObject(){
+    void get_GiveIntegerObjectInOf_ReturnIntegerObject() {
         Integer excepted = 10;
         Integer actual = Optional.of(excepted).get();
 
